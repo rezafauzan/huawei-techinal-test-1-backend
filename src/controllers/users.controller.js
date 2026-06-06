a96d5a0b8790205e506516c58e905cebf10b8b12
rezafauzan <rezafauzan593@gmail.com>
2026-06-06 17:03:23 +0700

    Feat: add createUsers user controller function

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -24,3 +24,93 @@ export async function getAllUsers(request, response) {
         )
     }
 }
+
+/**
+ * 
+ * @param {import("express").Request} request 
+ * @param {import("express").Response} response 
+ */
+export async function createUsers(request, response) {
+    const {
+        first_name,
+        last_name,
+        address,
+        phone,
+        email,
+        password,
+        confirm_password
+    } = request.body
+
+    if (first_name === undefined || first_name.length < 4) {
+        return httpResponse.badRequest(
+            response,
+            "Create user failed : First Name minimum 4 characters"
+        )
+    }
+
+    if (last_name === undefined || last_name.length < 4) {
+        return httpResponse.badRequest(
+            response,
+            "Create user failed : Last Name minimum 4 characters"
+        )
+    }
+
+    if (phone === undefined || phone.length < 10) {
+        return httpResponse.badRequest(
+            response,
+            "Create user failed : Phone Number minimum 10 digits"
+        )
+    }
+
+    if (address === undefined || address.length < 10) {
+        return httpResponse.badRequest(
+            response,
+            "Create user failed : Address minimum 10 characters"
+        )
+    }
+
+    if (email === undefined || !email.includes("@")) {
+        return httpResponse.badRequest(
+            response,
+            "Create user failed : Invalid email"
+        )
+    }
+
+    if (password === undefined || password.length < 8) {
+        return httpResponse.badRequest(
+            response,
+            "Create user failed : Password too weak! minimum 8 characters"
+        )
+    }
+
+    if (confirm_password !== password) {
+        return httpResponse.badRequest(
+            response,
+            "Create user failed : Confirm password not match"
+        )
+    }
+
+    try {
+        const newUser = {
+            first_name,
+            last_name,
+            address,
+            phone,
+            email,
+            password
+        }
+        userModel.createUsers(newUser)
+
+        return httpResponse.created(
+            response,
+            "Create user success!",
+            registeredUser
+        )
+
+    } catch (error) {
+        return httpResponse.serverError(
+            response,
+            "Create users fail! " + error
+        )
+    }
+}
