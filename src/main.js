8a015f5a3f2174222098fb677ba46147fe120060
rezafauzan <rezafauzan593@gmail.com>
2026-06-06 11:59:23 +0700

    Init: Initialize Express Rest API  Project

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import express from "express"
+import {constants} from "node:http2"
+
+const app = express()
+const port = process.env.PORT || 8888
+
+app.get("/", function(request, respond){
+    console.log("OK")
+    respond.status(constants.HTTP_STATUS_OK).json({
+        success: true,
+        message: "Backend is running well",
+        result: []
+    })
+})
+
+app.listen(port, function(){
+    console.log(`App listening on port ${port}`)
+})
