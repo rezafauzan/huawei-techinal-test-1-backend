87cd4707d3a4d0b07fbcae7e8fe255a94742ba4d
rezafauzan <rezafauzan593@gmail.com>
2026-06-06 16:57:43 +0700

    Feat: add users controller and getAllUsers controller function

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,23 @@
+import { httpResponse } from "../lib/http_handlers.js"
+import * as userModel from "../models/user.model.js"
+
+/**
+ * 
+ * @param {import("express").Request} request 
+ * @param {import("express").Response} response 
+ */
+export async function getAllUsers(request, response) {
+    try {
+        const users = await userModel.getAllUsers()
+        return httpResponse.ok(
+            response,
+            "Get all users data",
+            users
+        )
+    } catch (error) {
+        return httpResponse.serverError(
+            response,
+            "Failed get all users data " + error
+        )
+    }
+}
