6b0642c546676d1713d039f808dbe133de001ebc
rezafauzan <rezafauzan593@gmail.com>
2026-06-06 11:50:53 +0700

    Feat: user routers now using logMiddleware

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import * as userController from "../controllers/users.controller.js"
+import { logMiddleware } from "../middleware/log.middleware.js";
 
 const userRouter = Router()
 
-userRouter.get("", userController.getAllUsers)
-userRouter.post("", userController.createUsers)
-userRouter.delete("/:id", userController.deleteUser)
-userRouter.patch("/:id", userController.updateUser)
+userRouter.get("", logMiddleware, userController.getAllUsers)
+userRouter.post("", logMiddleware, userController.createUsers)
+userRouter.delete("/:id", logMiddleware, userController.deleteUser)
+userRouter.patch("/:id", logMiddleware, userController.updateUser)
 
 export default userRouter
