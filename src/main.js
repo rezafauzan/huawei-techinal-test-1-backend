ea2303e57b95817bbe3dd200abf96fe7fa289fdc
rezafauzan <rezafauzan593@gmail.com>
2026-06-06 12:08:01 +0700

    Feat: add swagger dependencies and add health check open api jsdoc

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import {constants} from "node:http2"
 const app = express()
 const port = process.env.PORT || 8888
 
+/**
+ * @openapi
+ * /:
+ *  get:
+ *      tags: ['Health Check']
+ *      description: Health Check
+ *      responses:
+ *          200:
+ *              description: Returning JSON with success and message
+ */
 app.get("/", function(request, respond){
     console.log("OK")
     respond.status(constants.HTTP_STATUS_OK).json({
