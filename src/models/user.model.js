4fd796dd1e207137440ce28b4b4f6b1b80b94646
rezafauzan <rezafauzan593@gmail.com>
2026-06-06 11:34:41 +0700

    Refactor: usermodel functions export into one line

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,26 +1,28 @@
 import { users } from "../../storage/data.js"
 
-export function getAllUsers(){
+function getAllUsers(){
     return users
 }
 
-export function createUsers(user){
+function createUsers(user){
     user.id = users.length
     users.push(user)
     return user
 }
 
-export function deleteUser(id){
+function deleteUser(id){
     const index = users.findIndex(user, index => user.id === parseInt(id))
     const deletedUser = users[index]
     users.splice(index, 1)
     return deletedUser
 }
 
-export function updateUsers(newUserData){
+function updateUsers(newUserData){
     const user = users.find(user, index => user.id === parseInt(newUserData.id))
     if(!user){
         return null
     }
     users[user.id] = newUserData
 }
+
+export {getAllUsers, createUsers, deleteUser, updateUsers}
